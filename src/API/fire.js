3e4fdf00493be07555333b4835030bcead874478
Jestero <jesterowtf@gmail.com>
2022-11-03 11:57:54 +0500

    fix Header

--- src/API/fire.js
+++ src/API/fire.js
@@ -1,24 +1,24 @@
 import firebase from "firebase/compat/app";
 
-// export const fireDB = firebase.initializeApp({
-//     apiKey: process.env.REACT_APP_API_KEY,
-//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//     projectId: process.env.REACT_APP_PROJECT_ID,
-//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//     appId: process.env.REACT_APP_APP_ID,
-//     measurementId: process.env.REACT_APP_MEASUREMENT_ID
-//   }
-// );
-
 export const fireDB = firebase.initializeApp({
-      apiKey: "AIzaSyCC121bbMH0TKqouju7PbJZXLvWn4HvfoI",
-      authDomain: "main-c423b.firebaseapp.com",
-      projectId: "main-c423b",
-      storageBucket: "main-c423b.appspot.com",
-      messagingSenderId: "270644061651",
-      appId: "1:270644061651:web:c4ffebab13b57637817b26",
-      measurementId: "G-ZPL8NNS5FK"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   }
 );
+
+// export const fireDB = firebase.initializeApp({
+//       apiKey: "AIzaSyCC121bbMH0TKqouju7PbJZXLvWn4HvfoI",
+//       authDomain: "main-c423b.firebaseapp.com",
+//       projectId: "main-c423b",
+//       storageBucket: "main-c423b.appspot.com",
+//       messagingSenderId: "270644061651",
+//       appId: "1:270644061651:web:c4ffebab13b57637817b26",
+//       measurementId: "G-ZPL8NNS5FK"
+//   }
+// );
 
